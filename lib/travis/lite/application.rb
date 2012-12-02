91b425aaacd7c3d3298723c7c4644002ee0e9026
Henrik Hodne <me@henrikhodne.com>
2012-12-02 11:49:47 -0600

    Add repository show view

--- lib/travis/lite/application.rb
+++ lib/travis/lite/application.rb
@@ -4,11 +4,14 @@ Bundler.require
 require 'net/https'
 require 'open-uri'
 
+require 'travis/lite/models/build'
 require 'travis/lite/models/repository'
 
 module Travis
   module Lite
     class Application < Sinatra::Base
+      include Models
+
       configure :production do
         disable :raise_exceptions
         disable :show_exceptions
@@ -22,9 +25,43 @@ module Travis
 
       get '/' do
         repositories_json = JSON.parse(open('https://api.travis-ci.org/repos/').read)
-        @repositories = repositories_json.map { |repository| Models::Repository.new(repository) }
+        @repositories = repositories_json.map { |repository| Repository.new(repository) }
 
         erb :index
+      end
+
+      get '/:user/:repo' do |user, repo|
+        slug = "#{user}/#{repo}"
+        repository_json = JSON.parse(open("https://api.travis-ci.org/repos/#{slug}").read)
+        repository = Repository.new(repository_json)
+
+        @slug = repository.slug
+        @build_number = repository.last_build_number
+        if repository.last_build_finished?
+          @build_status = repository.last_build_passed? ? 'Passed' : 'Failed'
+        else
+          @build_status = 'Running'
+        end
+
+        builds_json = JSON.parse(open("https://api.travis-ci.org/repos/#{slug}/builds").read)
+
+        @builds = builds_json.map do |build_json|
+          Build.new(build_json)
+        end.map do |build|
+          if build.finished?
+            build_status = build.passed? ? 'Passed' : 'Failed'
+          else
+            build_status = 'Running'
+          end
+          {
+            number: build.number,
+            status: build_status,
+          }
+        end
+
+        # @builds = JSON.pretty_generate(JSON.parse(open("https://api.travis-ci.org/repos/#{slug}/builds").read))
+
+        erb :repository
       end
 
       error do
