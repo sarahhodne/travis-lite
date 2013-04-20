27d46f96670416a1b06595e6fc5411feeb41c337
Henrik Hodne <me@henrikhodne.com>
2013-04-20 02:58:27 -0500

    Use the travis client library

--- lib/travis/lite/views/build.rb
+++ lib/travis/lite/views/build.rb
@@ -13,27 +13,36 @@ module Travis
         end
 
         def matrix
-          @build.matrix.map { |matrix| convert_matrix(matrix) }
+          @build.jobs.map { |job| convert_job(job) }
         end
 
         private
 
-        def convert_matrix(matrix)
+        def convert_job(job)
           {
-            id: matrix.id,
-            number: matrix.number,
-            status: format_build_status(matrix_status(matrix)),
-            config_label: matrix.config.label,
-            allow_failure: matrix.allow_failure?,
-            row_class: class_for_build_status(matrix_status(matrix))
+            id: job.id,
+            number: job.number,
+            status: format_build_status(job_status(job)),
+            config_label: job_label(job.config),
+            allow_failure: job.allow_failure?,
+            row_class: class_for_build_status(job_status(job))
           }
         end
 
         private
 
-        def matrix_status(matrix)
-          if matrix.finished?
-            matrix.passed? ? :passed : :failed
+        def job_label(config)
+          keys = %w[rvm compiler env]
+          keys << config['language']
+
+          sub = config.values_at(keys).compact.join(" ")
+
+          "#{config['language']} #{sub}"
+        end
+
+        def job_status(job)
+          if job.finished?
+            job.passed? ? :passed : :failed
           else
             :running
           end
