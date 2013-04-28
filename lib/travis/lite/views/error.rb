22c34aee6c408cd86499ac7e020e7c27ae8d8597
Henrik Hodne <me@henrikhodne.com>
2013-04-27 19:21:07 -0500

    Add error templates

---
+++ lib/travis/lite/views/error.rb
@@ -0,0 +1,10 @@
+require 'travis/lite/views/layout'
+
+module Travis
+  module Lite
+    module Views
+      class Error < Layout
+      end
+    end
+  end
+end
---
+++ lib/travis/lite/views/not_found.rb
@@ -0,0 +1,10 @@
+require 'travis/lite/views/layout'
+
+module Travis
+  module Lite
+    module Views
+      class NotFound < Layout
+      end
+    end
+  end
+end
