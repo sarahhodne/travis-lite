6e48a895a051a17965423c749b6041cb4cc05d5a
Peter Suschlik <ps@neopoly.de>
2013-08-02 10:34:08 +0200

    Allow repository names to contain dots.
    Before this commit repository names which contain dots point
    to wrong repositories.
    
    Examples:
    * http://travis-lite.com/henrikhodne/davinci.rb -> henrikhodne/davinci
    * http://travis-lite.com/kytrinyx/exercism.io -> kytrinyx/exercism
    
    Note that had to extract the show route as a member to make the regexp work.
    It didn't work on `resources :repositories`.

--- spec/routing/builds_routing_spec.rb
+++ spec/routing/builds_routing_spec.rb
@@ -10,6 +10,15 @@ describe "routing to builds" do
     )
   end
 
+  it "routes repository names containing dots correctly" do
+    expect(get: "/henrikhodne/davinci.rb/builds").to route_to(
+      controller: "builds",
+      action: "index",
+      owner_name: "henrikhodne",
+      repository_name: "davinci.rb",
+    )
+  end
+
   it "routes /henrikhodne/travis-lite/builds/12345 to builds#show" do
     expect(get: "/henrikhodne/travis-lite/builds/12345").to route_to(
       controller: "builds",
--- spec/routing/jobs_routing_spec.rb
+++ spec/routing/jobs_routing_spec.rb
@@ -10,4 +10,14 @@ describe "routing to jobs" do
       id: "12345",
     )
   end
+
+  it "routes repository names containing dots correctly" do
+    expect(get: "/henrikhodne/davinci.rb/jobs/12345").to route_to(
+      controller: "jobs",
+      action: "show",
+      owner_name: "henrikhodne",
+      repository_name: "davinci.rb",
+      id: "12345",
+    )
+  end
 end
